a596267effc844cc1eb8665f774bcca34aa68cc4
Oidaho <76991612+Oidaho@users.noreply.github.com>
2026-09-24 12:38:24 +1000

    feat(domain): add the repository layer with the Redis repository

---
+++ src/domain/repositories/__init__.py
@@ -0,0 +1,4 @@
+from ._abc import AbstractRepository
+from ._exc import RepositoryError
+
+__all__ = ("AbstractRepository", "RepositoryError")
---
+++ src/domain/repositories/_exc.py
@@ -0,0 +1,5 @@
+from src.general.exceptions import DomainError
+
+
+class RepositoryError(DomainError):
+    """Ошибка работы репозитория."""
