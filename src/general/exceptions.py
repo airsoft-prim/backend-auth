909bdcc91b0305316b7bb8b3701dc4f8cacb0319
Oidaho <76991612+Oidaho@users.noreply.github.com>
2026-09-25 01:46:10 +1000

    feat(general): add the service and provider error classes

--- src/general/exceptions.py
+++ src/general/exceptions.py
@@ -4,3 +4,15 @@ class AuthBackendError(Exception):
 
 class DomainError(AuthBackendError):
     """Ошибка выполнения бизнес-логики."""
+
+
+class ServiceError(DomainError):
+    """Ошибка в работе служб."""
+
+
+class ProviderError(AuthBackendError):
+    """Ошибка выполнения инфраструктурной логики."""
+
+
+class RepositoryError(ProviderError):
+    """Ошибка в работе репозиториев."""
